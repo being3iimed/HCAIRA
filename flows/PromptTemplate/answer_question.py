180be935b78891b3a4a9947ff0ca48a51cbd8f8f
Imed Kouki <88271652+being3iimed@users.noreply.github.com>
2024-08-16 11:42:22 +0000

    refactoring answer_question.py

--- flows/PromptTemplate/answer_question.py
+++ flows/PromptTemplate/answer_question.py
@@ -1,21 +1,18 @@
 import os
 from langchain.prompts import PromptTemplate
 
-# Genrating queries in ReliefWeb Template
+# Generating queries in ReliefWeb Template
 answer_question = f'''
 system:
-You are a helpful assistant. Using the output from a query to reliefweb, anser the user's question.
-You always provide your sources when answering a question, providing the report name, link and quote the relevant information.
+You are a helpful assistant. Using the output from a query to ReliefWeb, answer the user's question.
+You always provide your sources when answering a question, providing the report name, link, and quoting the relevant information.
 
 {{reliefweb_data}}
 
-{% for item in chat_history %}
 user:
-{{item.inputs.question}}
-assistant:
-{{item.outputs.answer}}
-{% endfor %}
+{{question}}
+'''
 
-user:
-{{question}}'''
 answer_question_prompt = PromptTemplate.from_template(answer_question)
+
+print(answer_question_prompt.format(question='nothing', reliefweb_data='fun'))
